663635b10c16912db2a3852eeaf49f42ef77d15a
Juan Ignacio Torres <juan.torres@despegar.com>
2021-08-01 00:01:33 -0300

    UPDATE
    * Se puede pasar el puerto por variables de ambiente

--- app.go
+++ app.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/JuanigTorres/xmen-finder/controller"
 	"github.com/JuanigTorres/xmen-finder/database"
@@ -13,9 +14,14 @@ import (
 //  - Agregar README.md
 //  - Rename go.mod
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9290"
+	}
+
 	database.NewClient()
 	defer database.Disconnect()
 	http.HandleFunc("/mutant", controller.MutantHandler)
 	http.HandleFunc("/stats", controller.StatsHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
